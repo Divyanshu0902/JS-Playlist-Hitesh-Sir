689afcf4a7c9a34d47395e3bd0de8df43284946d
Divyanshu Kumar <divyanshu.kumar.it28@heritageit.edu.in>
2025-09-19 21:21:25 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,21 @@
+const acc_id=12345;
+let acc_email="abc@gmail.com";
+var acc_password="Vkdp@0306";
+acc_city="Kolkata"
+
+console.log(acc_email);
+console.log(acc_password);
+console.log(acc_city);
+
+
+// acc_id=54321;
+acc_email="xyz@gmail.com";
+acc_password="RDkr@0902";
+acc_city="Kota";
+
+console.log(acc_email);
+console.log(acc_password);
+console.log(acc_city);
+let acc_state;
+
+console.table([acc_email,acc_password,acc_city,acc_state]);
