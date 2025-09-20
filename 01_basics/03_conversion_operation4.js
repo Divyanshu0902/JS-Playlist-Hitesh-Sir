147255c547e0dba1fb06fdcd0b39c55a927df305
Divyanshu Kumar <divyanshu.kumar.it28@heritageit.edu.in>
2025-09-20 00:09:30 +0000

    conversion into string

---
+++ 01_basics/03_conversion_operation4.js
@@ -0,0 +1,17 @@
+// --- Conversion into String ---
+let number = 902
+let stringNumber = String(number)
+console.log(stringNumber)
+
+let bool = true
+let stringBool = String(bool)
+console.log(stringBool)
+
+let value=null
+let stringNull = String(value)
+console.log(stringNull)
+
+//let initalVar= undefined 
+            //this causes error! bcoz null/true are considered as a value but undefined is considered as a property rather than a value.
+//let changedVar= String(initialVar)
+//console.log(chngedVar);
