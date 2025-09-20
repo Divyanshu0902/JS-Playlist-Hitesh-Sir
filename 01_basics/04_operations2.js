9333d5ef46154cb9a71e67541e6b68f3b8566510
Divyanshu Kumar <divyanshu.kumar.it28@heritageit.edu.in>
2025-09-20 08:48:11 +0000

    operations2

---
+++ 01_basics/04_operations2.js
@@ -0,0 +1,17 @@
+//---String Operations---
+
+// let str1 = "Hello "
+// let str2 = "Divyanshu"
+// let str3 = str1+str2
+// console.log(str3)  //op-- Hello Divyanshu
+
+// console.log("1" + "2")  //op-- 12
+// console.log("1" + 2);   //op-- 12
+// console.log(1 + "2");   //op-- 12
+
+console.log("1" + 2 + 2);   //op-- 122
+console.log(1 + "2" + 2);   //op-- 122
+console.log(1 + 2 + "2");   //op-- 32
+
+
+
