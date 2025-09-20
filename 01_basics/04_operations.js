734a1f08e7999a9071f735ef60fbe4df4d5975bf
Divyanshu Kumar <divyanshu.kumar.it28@heritageit.edu.in>
2025-09-20 00:45:42 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,23 @@
+// ---varios operations---
+
+//---UNARY Operations---
+let n = 3
+NegateN = -n
+console.log(NegateN)
+
+let m = 5
+m++  //or we can write: m=m+1
+console.log(m)  //op-- 6
+
+let l = 11
+l--  //or we can write: l=l-1
+console.log(l)  //op-- 10
+
+
+//---Other Operations---
+console.log(3+2)
+console.log(3-2)
+console.log(3*2)
+console.log(10/3)   //quotient with decimal fraction.
+console.log(3%2)   //remainder
+console.log(3**2)  //exponent
