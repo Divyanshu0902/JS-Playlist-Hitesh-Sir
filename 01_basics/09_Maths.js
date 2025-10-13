d0f98dd99d309256e6328e748bc9c91cfe9f968f
Divyanshu1306 <divyanshukr.jeema@gmail.com>
2025-10-13 19:47:49 +0530

    Math Library of JS

---
+++ 01_basics/09_Maths.js
@@ -0,0 +1,37 @@
+// Math is a JS Library consisting of many math related functions.
+
+// console.log(Math.abs(-5))   // Absolute value
+
+
+// console.log(Math.round(4.6)) // Rounds to nearest integer
+// console.log(Math.ceil(4.2))  // Rounds up  i.e. 5
+// console.log(Math.floor(4.7)) // Rounds down i.e. 4
+
+// console.log(Math.pow(2, 3))
+// console.log(Math.sqrt(16))
+
+// console.log(Math.min(2, 3, 1, 4, -5, 6))    //-5
+// console.log(Math.max(2, 3, 1, 4, -5, 6))    //6
+
+
+//------------------------------------------------------------------------------------------------------
+
+// console.log(Math.random());  // Random number between 0 and 1
+
+// console.log(Math.floor(Math.random() * 10)); // Random number between 0 and 9
+// console.log(Math.floor(Math.random() * 10) + 1); // Random number between 1 and 10
+// console.log(Math.floor(Math.random() * 100) + 1); // Random number between 1 and 100
+
+
+//------------------------------------------------------------------------------------------------------
+
+// const min = 10;
+// const max = 20;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min); // Random number between min and max
+
+// Ex-  for 3 to 19
+console.log(Math.floor(Math.random()*17)+3)     //generates numbers randomly between 3 and 19
+
+//------------------------------------------------------------------------------------------------------
+
