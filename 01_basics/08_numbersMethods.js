67f9b79078dc28f5443a00d76557e0c478f9d5dc
Divyanshu1306 <divyanshukr.jeema@gmail.com>
2025-10-13 16:31:28 +0530

    number and its methods

---
+++ 01_basics/08_numbersMethods.js
@@ -0,0 +1,39 @@
+// const score = 400;
+
+// console.log(score.toString());  // 400, type- String
+// console.log(score.toFixed(2)); // 400.00, type- Number
+
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+// const num = 1234.56789;
+
+// console.log(num.toPrecision(8));    // 1234.5679
+// console.log(num.toPrecision(7));    // 1234.568
+// console.log(num.toPrecision(6));    // 1234.57
+// console.log(num.toPrecision(5));    // 1234.6
+// console.log(num.toPrecision(4));    // 1235
+// console.log(num.toPrecision(3));    // 1.23e+3
+// console.log(num.toPrecision(2));    // 1.2e+3
+// console.log(num.toPrecision(1));    // 1e+3
+// console.log(num.toPrecision(0));    // RangeError: toPrecision() argument must be between 1 and 21
+
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString());          // 1,000,000
+// console.log(hundreds.toLocaleString("en-US"));   // 1,000,000
+
+// console.log(hundreds.toLocaleString("en-IN"));   // 10,00,000
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+
